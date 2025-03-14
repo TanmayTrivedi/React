a80967571ea15f536d074ee2e84a6b7bbef6daee
TanmayTrivedi <tanmayrishu1112@gmail.com>
2025-03-15 00:00:30 +0530

    file added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+import Demo from "./demo"
+
+function App() {
+
+  return (
+   <Demo/>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/demo.jsx
@@ -0,0 +1,7 @@
+function Demo(){
+    return (
+       <h2> My name is tanmay</h2>
+    )
+}
+
+export default Demo
